9b989d65144484503b8cd2c0d0966cf2a7a62f72
Sugan S <135805343+sugansvs@users.noreply.github.com>
2026-05-18 09:40:43 +0530

    Add company tracks module

---
+++ modules/company_tracks.py
@@ -0,0 +1,7 @@
+def get_company_tracks():
+    return {
+        'TCS':['SQL','Excel','Communication'],
+        'Infosys':['SQL','Power BI'],
+        'Zoho':['Product Analytics','SQL'],
+        'Remote Startup':['Python','Metrics','Experimentation']
+    }
